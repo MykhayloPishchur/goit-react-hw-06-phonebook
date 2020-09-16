f90b1a3113e09f665ffa3c35478fb7c399ff440e
unknown <you@example.com>
2020-09-17 00:16:25 +0300

    fix

--- src/Redux/Reducers/contacts.js
+++ src/Redux/Reducers/contacts.js
@@ -8,17 +8,14 @@
 const initialState = () => {
   if (localStorage.getItem("contacts")) {
     return JSON.parse(localStorage.getItem("contacts"));
-  } else return [];
+  } else
+    return [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ];
 };
-
-// const initialState = () => {
-//   const actualContacts = localStorage.getItem("contacts");
-
-//   if (actualContacts) {
-//     const contacts = JSON.parse(actualContacts);
-//     return contacts;
-//   }
-// };
 
 const phoneBookReducer = (state = initialState(), { type, payload }) => {
   switch (type) {
